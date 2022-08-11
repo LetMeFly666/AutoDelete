489dca8ee1da525220211cf87f909d9bef75635d
LetMeFly666 <814114971@qq.com>
2022-08-11 16:34:50 +0800

    .text -> .string

--- ChangeHtml.py
+++ ChangeHtml.py
@@ -10,7 +10,7 @@ def change1html(path):
     soup = BeautifulSoup(open(path, "r", encoding="utf-8").read(), "lxml")
     head = soup.find("head")
     scriptTag = soup.new_tag(name="script")
-    scriptTag.text = "window._LINKS_COMMONJS_NOTCHANGEICO = true;"
+    scriptTag.string = "window._LINKS_COMMONJS_NOTCHANGEICO = true;"
     head.append(scriptTag)
     scriptTag = soup.new_tag("script", src="https://letmefly.xyz/Links/Common.js")
     head.append(scriptTag)
