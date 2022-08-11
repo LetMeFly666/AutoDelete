eb71afea602844ea41c88ff850234680a400d499
LetMeFly666 <814114971@qq.com>
2022-08-11 12:17:42 +0800

    custom change

---
+++ ChangeHtml.py
@@ -0,0 +1,13 @@
+'''
+Author: LetMeFly
+Date: 2022-08-11 12:13:08
+LastEditors: LetMeFly
+LastEditTime: 2022-08-11 12:16:05
+'''
+from bs4 import BeautifulSoup
+
+def change1html(path):
+    soup = BeautifulSoup(open(path, "r", encoding="utf-8").read())
+    print(soup)
+
+change1html("_site/index.html")
