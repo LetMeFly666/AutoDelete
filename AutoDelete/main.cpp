f2b0a85f9ab630396805d4b34d2bf31021ae9c50
LetMeFly666 <814114971@qq.com>
2022-08-11 21:34:06 +0800

    Init Hello World

---
+++ AutoDelete/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Author: LetMeFly
+ * @Date: 2022-08-11 21:29:48
+ * @LastEditors: LetMeFly
+ * @LastEditTime: 2022-08-11 21:32:38
+ */
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
