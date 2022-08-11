2a54134752a99a16e22ee6f46afd167a7805ce15
LetMeFly666 <814114971@qq.com>
2022-08-11 12:33:18 +0800

    change

--- ChangeHtml.py
+++ ChangeHtml.py
@@ -2,15 +2,26 @@
 Author: LetMeFly
 Date: 2022-08-11 12:13:08
 LastEditors: LetMeFly
-LastEditTime: 2022-08-11 12:19:51
+LastEditTime: 2022-08-11 12:33:13
 '''
-import os
-print(os.system("tree"))
-
 from bs4 import BeautifulSoup
 
 def change1html(path):
-    soup = BeautifulSoup(open(path, "r", encoding="utf-8").read())
+    soup = BeautifulSoup(open(path, "r", encoding="utf-8").read(), "lxml")
+    head = soup.find("head")
+    scriptTag = BeautifulSoup.new_tag("script")
+    scriptTag.text = "window._LINKS_COMMONJS_NOTCHANGEICO = true;"
+    head.append(scriptTag)
+    scriptTag = BeautifulSoup.new_tag("script", src="https://letmefly.xyz/Links/Common.js")
+    head.append(scriptTag)
+    linkTag = BeautifulSoup.new_tag("link", attrs={
+        "type": "image/x-icon",
+        "rel": "shortcut icon",
+        "href": "/favicon.ico"
+    })
+    head.append(linkTag)
     print(soup)
+    open(path, "w", encoding="utf-8").write(soup.prettify("utf-8"))
+
 
 change1html("_site/index.html")
