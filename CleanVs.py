25aecc49c0d483f260011b13779808b93a88150c
LetMeFly666 <814114971@qq.com>
2022-08-15 08:32:59 +0800

    And the clean VS

--- CleanVs.py
+++ CleanVs.py
@@ -2,10 +2,12 @@
 Author: LetMeFly
 Date: 2022-08-11 21:26:08
 LastEditors: LetMeFly
-LastEditTime: 2022-08-11 21:26:08
+LastEditTime: 2022-08-15 08:32:39
 '''
 import os
-toRemoveDir = [".vs", "x64", "x86"]
+toRemoveDir = [".vs", "x64", "x86", "Release"]
+
+print("Warn: donnot have directories named of", toRemoveDir)
 for i in os.walk('.'):
     dirPath = i[0]
     dirName = os.path.split(dirPath)[1]
